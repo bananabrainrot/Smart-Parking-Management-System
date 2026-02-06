09c735444d83bb93e8e84f658184ab6a1b06eab4
bananabrainrot <253751@astanait.edu.kz>
2026-02-06 11:29:06 +0500

    changes in url and user

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -10,8 +10,8 @@ import java.util.Properties;
 
 public class DatabaseConnection {
     private static final String URL =
-            "jdbc:postgresql://aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
-    private static final String USER = "postgres.mrivkaqomaorywhzsntk";
+            "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.foycqfivqdatcewmblsc";
     private static final String PASSWORD = loadPassword();
 
 
